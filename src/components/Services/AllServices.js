599474585f72d2881d66be6196f8cff2455cb034
ITDoctor40148 <zeus40148@hotmail.com>
2021-02-17 03:38:26 +0100

    test react slick for mobille

--- src/components/Services/AllServices.js
+++ src/components/Services/AllServices.js
@@ -12,19 +12,29 @@ import {
   MDBRow,
 } from "mdbreact"
 import { useMediaQuery } from 'react-responsive'
-import Flickity from 'react-flickity-component'
+// import Flickity from 'react-flickity-component'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import './flickity.css'
 import classes from "./AllServices.module.css"
 import renderHTML from "react-render-html"
 import Img from 'gatsby-image'
-import AniLink from "gatsby-plugin-transition-link/AniLink"
+// import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 const Allservices = () => {
   const MAX_LENGTH = 83
   const isTabletOrMobileDevice = useMediaQuery({
     query: '(max-device-width: 1224px)'
   })
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  };
   console.log(isTabletOrMobileDevice)
   return (
     <StaticQuery
@@ -53,66 +63,75 @@ const Allservices = () => {
           }
         }
       `}
-      render={props => (
-        <React.Fragment>
-          <MDBContainer className={classes.Con}>
-            {!isTabletOrMobileDevice || true ? (<MDBRow className={classes.Row}>
-              {props.allWordpressWpServices.edges.map(edge => {
-                return (
-                  (<MDBCol md="4" key={edge.node.slug}>
-                    <MDBCard className={classes.Card}>
-                      <Img
-                        // check edge has property
-                        fluid={edge.node ? edge.node.acf.header_img.localFile.childImageSharp.fluid : ''}
-                        waves
-                        className={classes.CardImg}
-                      />
-                      <MDBCardBody>
-                        <MDBCardTitle className={classes.CTitle}>{renderHTML(edge.node.title)}</MDBCardTitle>
+      render={props => {
+        // console.log(props);
+        return (
+          <React.Fragment>
+            <MDBContainer className={classes.Con}>
+              {!isTabletOrMobileDevice ? (<MDBRow className={classes.Row}>
+                {props.allWordpressWpServices.edges.map(edge => {
+                  if (edge.node.acf.header_img.localFile) {
+                    return (
+                      (<MDBCol md="4" key={edge.node.slug}>
+                        <MDBCard className={classes.Card}>
+                          <Img
+                            // check edge has property
+                            fluid={edge.node.acf.header_img.localFile ? edge.node.acf.header_img.localFile.childImageSharp.fluid : ''}
+                            waves
+                            className={classes.CardImg}
+                          />
+                          <MDBCardBody>
+                            <MDBCardTitle className={classes.CTitle}>{renderHTML(edge.node.title)}</MDBCardTitle>
 
-                        <span className="card-text">
-                          {renderHTML(edge.node.excerpt)}
-                        </span>
+                            <span className="card-text">
+                              {renderHTML(edge.node.excerpt)}
+                            </span>
 
-                        <AniLink paintDrip duration={1.2} color='green' direction="up" to={`/services/${edge.node.slug}`}>
+                            {/* <AniLink paintDrip duration={1.2} color='green' direction="up" to={`/services/${edge.node.slug}`}>
                           <MDBBtn color="black">Read More</MDBBtn>
-                        </AniLink>
-                      </MDBCardBody>
-                    </MDBCard>
-                  </MDBCol>)
-                )
-              })}
-            </MDBRow>) : (
-                <Flickity
-                  className={'carousel'}
-                  elementType={'div'} // default 'div'
-                >
-                  {props.allWordpressWpServices.edges.map(edge =>
-                    <MDBCard className={classes.Card} key={edge.node.slug}>
-                      <Img
-                        // check edge has property
-                        fluid={edge.node ? edge.node.acf.header_img.localFile.childImageSharp.fluid : ''}
-                        waves
-                        className={classes.CardImg}
-                      />
-                      <MDBCardBody>
-                        <MDBCardTitle className={classes.CTitle}>{renderHTML(edge.node.title)}</MDBCardTitle>
+                        </AniLink> */}
+                          </MDBCardBody>
+                        </MDBCard>
+                      </MDBCol>)
+                    )
+                  } else {
+                    return
+                  }
 
-                        <span className="card-text">
-                          {renderHTML(edge.node.excerpt)}
-                        </span>
+                })}
+              </MDBRow>) : (
+                  <Slider
+                    className={'carousel'}
+                    elementType={'div'} // default 'div'
+                    {...settings}
+                  >
+                    {props.allWordpressWpServices.edges.map(edge =>
+                      <MDBCard className={classes.Card} key={edge.node.slug}>
+                        <Img
+                          // check edge has property
+                          fluid={edge.node ? edge.node.acf.header_img.localFile.childImageSharp.fluid : ''}
+                          waves
+                          className={classes.CardImg}
+                        />
+                        <MDBCardBody>
+                          <MDBCardTitle className={classes.CTitle}>{renderHTML(edge.node.title)}</MDBCardTitle>
 
-                        <AniLink paintDrip duration={1.2} color='green' direction="up" to={`/services/${edge.node.slug}`}>
+                          <span className="card-text">
+                            {renderHTML(edge.node.excerpt)}
+                          </span>
+
+                          {/* <AniLink paintDrip duration={1.2} color='green' direction="up" to={`/services/${edge.node.slug}`}>
                           <MDBBtn color="black">Read More</MDBBtn>
-                        </AniLink>
-                      </MDBCardBody>
-                    </MDBCard>
-                  )}
-                </Flickity>
-              )}
-          </MDBContainer>
-        </React.Fragment>
-      )}
+                        </AniLink> */}
+                        </MDBCardBody>
+                      </MDBCard>
+                    )}
+                  </Slider>
+                )}
+            </MDBContainer>
+          </React.Fragment>
+        )
+      }}
     ></StaticQuery>
   )
 }
