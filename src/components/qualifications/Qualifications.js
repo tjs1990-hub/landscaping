67e4a27a15f50e3eca6162dfe0e021195ae08e15
ITDoctor40148 <zeus40148@hotmail.com>
2021-02-24 11:27:21 +0100

    pwa

--- src/components/qualifications/Qualifications.js
+++ src/components/qualifications/Qualifications.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { graphql, StaticQuery } from "gatsby"
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
+import { useBreakpoint } from 'gatsby-plugin-breakpoints';
 import classes from "./Quals.module.css"
 
 const Qualifications = () => {
+  const breakpoints = useBreakpoint();
   return (
     <StaticQuery
       query={graphql`
@@ -32,7 +34,7 @@ const Qualifications = () => {
       render={props => (
         <React.Fragment>
           <MDBContainer className={classes.Con}>
-            <MDBRow>
+            {!breakpoints.sm ? <MDBRow>
               <MDBCol md="4" className={classes.Col}>
                 <img
                   src={props.wordpressWpQualifications.acf.one.source_url}
@@ -54,7 +56,23 @@ const Qualifications = () => {
                   className={classes.Img}
                 />
               </MDBCol>
-            </MDBRow>
+            </MDBRow> : <div className={classes.scrollingWrapperFlexbox}>
+                <img
+                  src={props.wordpressWpQualifications.acf.one.source_url}
+                  alt={props.wordpressWpQualifications.acf.one.alt_text}
+                  className={`${classes.Img} mx-5`}
+                />
+                <img
+                  src={props.wordpressWpQualifications.acf.two.source_url}
+                  alt={props.wordpressWpQualifications.acf.two.alt_text}
+                  className={`${classes.Img} mx-5`}
+                />
+                <img
+                  src={props.wordpressWpQualifications.acf.three.source_url}
+                  alt={props.wordpressWpQualifications.acf.three.alt_text}
+                  className={`${classes.Img} mx-5`}
+                />
+              </div>}
           </MDBContainer>
         </React.Fragment>
       )}
